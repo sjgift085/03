7d5971893a145edebc336ad268e8484b1c752e73
sjgift085 <77606778+sjgift085@users.noreply.github.com>
2025-09-18 16:15:58 +0900

    실습 2 수정

--- main.c
+++ main.c
@@ -2,17 +2,16 @@
 #include <stdlib.h>
 
 int main(void){
-  
-  float a, b;
-  
-  printf("분자를 입력하세요 : ");
-  scanf("%f", &a);
-  
-  printf("분모를 입력하세요 : ");
-  scanf("%f", &b);
-  
-  printf("나누기의 결과는 %f 입니다", a/b); 
-  
-  system("PAUSE");	
-  return 0;
+	char c;
+	int i;
+	
+	printf("input a number : ");
+	scanf("%c", &c);
+
+    i = c - '0';
+	printf("The input number is %i\n", i);
+
+    system("pause");
+	return 0;
+
 }
